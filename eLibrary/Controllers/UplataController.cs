83f6a675d7de3f510026145c69fd498f2f60e0e0
Amina Kreso <amina.kreso@edu.fit.ba>
2021-09-17 01:40:07 +0200

    CRUD uplata

---
+++ eLibrary/Controllers/UplataController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using eLibrary.Models;
+
+namespace eLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UplataController : ControllerBase
+    {
+        private readonly qsd_eLibraryContext _context;
+
+        public UplataController(qsd_eLibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Uplata
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Uplata>>> GetUplata()
+        {
+            return await _context.Uplata.ToListAsync();
+        }
+
+        // GET: api/Uplata/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Uplata>> GetUplata(int id)
+        {
+            var uplata = await _context.Uplata.FindAsync(id);
+
+            if (uplata == null)
+            {
+                return NotFound();
+            }
+
+            return uplata;
+        }
+
+        // PUT: api/Uplata/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUplata(int id, Uplata uplata)
+        {
+            if (id != uplata.UplataId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(uplata).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UplataExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Uplata
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Uplata>> PostUplata(Uplata uplata)
+        {
+            _context.Uplata.Add(uplata);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUplata", new { id = uplata.UplataId }, uplata);
+        }
+
+        // DELETE: api/Uplata/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUplata(int id)
+        {
+            var uplata = await _context.Uplata.FindAsync(id);
+            if (uplata == null)
+            {
+                return NotFound();
+            }
+
+            _context.Uplata.Remove(uplata);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UplataExists(int id)
+        {
+            return _context.Uplata.Any(e => e.UplataId == id);
+        }
+    }
+}
