213fb2b841bca902b15b014f593d28ef3e53d83c
Amina Kreso <amina.kreso@edu.fit.ba>
2021-10-03 21:19:12 +0200

    Korisnik controller

---
+++ eLibrary/Controllers/KorisnikController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using eLibrary.Models;
+using eLibrary.ActionFilters;
+
+namespace eLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KorisnikController : ControllerBase
+    {
+        private readonly qsd_eLibraryContext _context;
+
+        public KorisnikController(qsd_eLibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Korisnik
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Korisnik>>> GetKorisnik()
+        {
+            return await _context.Korisnik.ToListAsync();
+        }
+
+        // GET: api/Korisnik/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Korisnik>> GetKorisnik(int id)
+        {
+            var korisnik = await _context.Korisnik.FindAsync(id);
+
+            if (korisnik == null)
+            {
+                return NotFound();
+            }
+
+            return korisnik;
+        }
+
+        // PUT: api/Korisnik/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+
+        public async Task<IActionResult> PutKorisnik(int id, Korisnik korisnik)
+        {
+            if (id != korisnik.KorisnikId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(korisnik).State = EntityState.Modified;
+            _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+
+        // POST: api/Korisnik
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+
+        public async Task<ActionResult<Korisnik>> PostKorisnik(Korisnik korisnik)
+        {
+            _context.Korisnik.Add(korisnik);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetKorisnik", new { id = korisnik.KorisnikId }, korisnik);
+        }
+
+        // DELETE: api/Korisnik/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteKorisnik(int id)
+        {
+            var korisnik = await _context.Korisnik.FindAsync(id);
+            if (korisnik == null)
+            {
+                return NotFound();
+            }
+
+            _context.Korisnik.Remove(korisnik);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool KorisnikExists(int id)
+        {
+            return _context.Korisnik.Any(e => e.KorisnikId == id);
+        }
+    }
+}
